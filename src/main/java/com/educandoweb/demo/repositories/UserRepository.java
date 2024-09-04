2944ca6ba80b2513a1241380dc803118fbefd1a6
Maria Mendes <mendes.maria@ipcbcampus.pt>
2024-09-04 16:04:32 +0100

    Service layer, component registration

--- src/main/java/com/educandoweb/demo/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/demo/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.educandoweb.demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.educandoweb.demo.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
