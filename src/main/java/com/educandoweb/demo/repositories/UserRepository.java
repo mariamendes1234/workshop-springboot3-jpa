ee4210e3babe0737854b39b5b353e8bec820960b
Maria Mendes <mendes.maria@ipcbcampus.pt>
2024-09-04 15:44:13 +0100

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/educandoweb/demo/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.educandoweb.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.demo.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	//JPA já faz a implementação!!!
+}
