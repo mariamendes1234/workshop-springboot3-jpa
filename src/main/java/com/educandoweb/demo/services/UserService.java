7b8f630f31fdd21ea4243f0b11bf0f77c98c4810
Maria Mendes <mendes.maria@ipcbcampus.pt>
2024-09-05 16:47:57 +0100

    Exception handling - findById

--- src/main/java/com/educandoweb/demo/services/UserService.java
+++ src/main/java/com/educandoweb/demo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.educandoweb.demo.entities.User;
 import com.educandoweb.demo.repositories.UserRepository;
+import com.educandoweb.demo.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -19,7 +20,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User u) {
