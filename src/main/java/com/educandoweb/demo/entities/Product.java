cad71e9c483179d5f8bcafee937bdfceace3bd5a
Maria Mendes <mendes.maria@ipcbcampus.pt>
2024-09-05 15:34:38 +0100

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/demo/entities/Product.java
+++ src/main/java/com/educandoweb/demo/entities/Product.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -33,6 +36,10 @@ public class Product implements Serializable {
 	inverseJoinColumns = @JoinColumn(name="category_id"))
 	private Set<Category> categories = new HashSet<>();
 
+	
+	@OneToMany(mappedBy="id.product")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Product() {
 	}
 
@@ -44,7 +51,16 @@ public class Product implements Serializable {
 		this.price = price;
 		this.imgUrl = imgUrl;
 	}
-
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
+		
+	}
+	
 	public Long getId() {
 		return id;
 	}
--- src/main/java/com/educandoweb/demo/repositories/OrderItemRepository.java
+++ src/main/java/com/educandoweb/demo/repositories/OrderItemRepository.java
@@ -3,7 +3,6 @@ package com.educandoweb.demo.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.demo.entities.OrderItem;
-import com.educandoweb.demo.entities.User;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
 
