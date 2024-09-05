690db6b7f2e9153dbf25a70c4a7a1698dfb73bb9
Maria Mendes <mendes.maria@ipcbcampus.pt>
2024-09-05 16:12:40 +0100

    User update

--- src/main/java/com/educandoweb/demo/services/UserService.java
+++ src/main/java/com/educandoweb/demo/services/UserService.java
@@ -12,20 +12,33 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll (){
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User u) {
 		return repository.save(u);
 	}
 	public void delete(long id) {
 		repository.deleteById(id);
+	}
+
+	public User update(long id, User u) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity,u);
+
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User u) {
+		entity.setName(u.getName());	
+		entity.setEmail(u.getEmail());
+		entity.setPhone(u.getPhone());
 	}
 }
